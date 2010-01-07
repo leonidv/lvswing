fd807de15a7d059a450a60398601c1d117687fef
Leonid Vygovskiy <devnull@localhost>
2010-01-07 13:16:00 +0300

    FileEdit java beans comments translate to English

--- src/main/java/com/vygovskiy/controls/fileedit/FileEdit.java
+++ src/main/java/com/vygovskiy/controls/fileedit/FileEdit.java
@@ -5,118 +5,90 @@ import java.awt.Dimension;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
- * Javabeans, который предоставляет доступ к выбору файла. При выборе файла
- * вызывается событие Action.
+ * Javabeans component for input file name.
  * 
- * @author leonidv
+ * @author Leonid Vygovskiy
  * 
  */
 public class FileEdit extends JPanel {
     private static final long serialVersionUID = 9166564872030028096L;
 
-    // Препочтительные размеры компоненты по ширине
+    /**
+     * Initial component preferred width.
+     */
     private static final int PREFERRED_WIDTH = 200;
 
-    // Преподчтительные размеры компоненты по высоте
+    /**
+     * Initial component preferred height.
+     */
     private static final int PREFERRED_HEIGHT = 20;
 
-    // Диалог для выбора файла
+    /**
+     * Dialog file chooser that showed whin {@link #button} is clicked.
+     */
     protected JFileChooser fileChooser;
 
-    // Поле ввода/отображения выбранного файла
+    /**
+     * Textfield with file name.
+     */
     protected JTextField textField;
 
-    // Кнопка, по которой вызывается каталог выбора файла
+    /**
+     * Button for show file chooser.
+     */
     protected JButton button;
 
-    // Расположение кнопки выбора файла относительно поля ввода
+    /**
+     * Position of button relative {@link #textField}.
+     */
     private ButtonPosition buttonPosition = ButtonPosition.East;
 
     /**
-     * Создает и размещает компоненты на панели.
-     * 
+     * Create and init components on panel.
      */
     private void init() {
-        // Инициализирем раскладку
         super.setLayout(new BorderLayout());
 
-        // Создаем компоненты
         textField = new JTextField();
         button = new JButton("Select file...");
 
-        // Создаем диалог выбора файла
         fileChooser = new JFileChooser();
 
-        // Добавляем созданные компоненты на панель
         add(textField, BorderLayout.CENTER);
         add(button, buttonPosition.getBorderLayoutConstant());
 
-        // Настраиваем действие кнопки
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 showDialog();
             }
         });
-        
-        // Создаем обработчик нажатия клавиши ENTER в строке ввода имени файла.
-        // Нажатием ENTER пользователь указывает на выбор файла.
-        textField.addKeyListener(new KeyListener(){
-            public void keyTyped(KeyEvent e) {};
-            public void keyPressed(KeyEvent e) {};
 
+        textField.addKeyListener(new KeyAdapter() {
             public void keyReleased(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     fireFileSelectedEvent();
                 }
-            }            
-        });
-    }
-
-    /**
-     * Показываем диалого выбора файла. Вызывается по нажатию кнопки. При этом
-     * если установлено корретное (существующее) имя файла,
-     * 
-     * 
-     */
-    private void showDialog() {
-        // Получаем файл, на который указывает компонента
-        File file = getFile();
-
-        // Если файл является директорией, устанавливаем его
-        if (file.isDirectory()) {
-            fileChooser.setSelectedFile(file);
-            // Иначе пытаемся установить директорию, в которой лежит файл и
-            // установить ее.
-        } else if ((file.getParentFile() != null)
-                && (file.getParentFile().exists())) {
-            fileChooser.setSelectedFile(file.getParentFile());
-        }
-
-        // Показываем диалог, и проверяем - выбрал пользователь файл или нет.
-        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-            try {
-                file = fileChooser.getSelectedFile();
-                setFileName(file.getCanonicalPath());
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
             }
-        }
+        });
+
+        setPreferredSize(new Dimension(PREFERRED_WIDTH, PREFERRED_HEIGHT));
     }
 
     /**
-     * Создает и инициализирует компоненту.
-     * 
+     * Create object without file selected.
      */
     public FileEdit() {
         super(new BorderLayout());
@@ -124,10 +96,35 @@ public class FileEdit extends JPanel {
     }
 
     /**
-     * Создает компоненту у устанавливает путь к файлу.
+     * Show fileChooser dialoger.
+     */
+    private void showDialog() {
+        File file = getFile();
+
+        /*
+         * Select right directory for showing in file chooser.
+         */
+        if (file.isDirectory()) {
+            fileChooser.setSelectedFile(file);
+        } else if ((file.getParentFile() != null)
+                && (file.getParentFile().exists())) {
+            fileChooser.setSelectedFile(file.getParentFile());
+        }
+
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            try {
+                file = fileChooser.getSelectedFile();
+                setFileName(file.getCanonicalPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Create component and set given file name.
      * 
-     * @param fileName -
-     *            заданное имя файла.
+     * @param fileName
      */
     public FileEdit(String fileName) {
         this();
@@ -135,84 +132,75 @@ public class FileEdit extends JPanel {
     }
 
     /**
-     * Возвращает указатель на кнопку выбора файла
+     * Return button that showing file chooser.
      * 
-     * @return кнопка открытия диалога выбора файла.
+     * @return
      */
     public JButton getButton() {
         return button;
     }
 
     /**
-     * Возвращает указатель на диалог выбора файла
+     * Return file chooser.
      * 
-     * @return диалог выбора файла
+     * @return
      */
     public JFileChooser getFileChooser() {
         return fileChooser;
     }
 
     /**
-     * Возвращает текстовое поле, в котором указывается имя файла
+     * Return text field with selected file name.
      * 
-     * @return текстовое поле с именем файла
+     * @return
      */
     public JTextField getTextField() {
         return textField;
     }
 
     /**
-     * Возвращает указанное имя файл, которое берется как строка из текстового
-     * поля. Для проверки, корректно указано имя файла или нет используйте метод
-     * checkFileName();
+     * Return content of text field.
      * 
-     * @return - возвращает имя файла, на которое указывает строка ввода.
+     * @return
+     * @see #getFile()
      */
     public String getFileName() {
         return textField.getText();
     }
 
     /**
-     * Устанавливает хранимое имя файла.
+     * Set value of text field and send {@link FileSelectedEvent}.
      * 
-     * @param fileName -
-     *            новое имя файла, на который ссылается компонент.
+     * @param fileName
      */
     public void setFileName(String fileName) {
-        String oldValue = textField.getText();
         textField.setText(fileName);
 
         fireFileSelectedEvent();
     }
 
     /**
-     * Возвращает файл, путь к которому указан в поле ввода.
+     * Return selected file.
      * 
-     * @return - файл, указанный пользователем.
      */
     public File getFile() {
         File file = new File(getFileName());
         return file;
     }
 
-    public Dimension getPreferredSize() {
-        return new Dimension(PREFERRED_WIDTH, PREFERRED_HEIGHT);
-    }
-
     /**
-     * Возвращает текущее положение кнопки относительно метки
+     * Return button position relative text field with file name.
      * 
-     * @return положение кнопки относительно поля ввода
+     * @return
      */
     public ButtonPosition getButtonPosition() {
         return buttonPosition;
     }
 
     /**
-     * Устанавливает положение кнопки относительно метки
+     * Set new button position relative to text field with file name.
      * 
-     * @param buttonPosition -
-     *            новое положение кнопки
+     * @param buttonPosition
      */
     public void setButtonPosition(ButtonPosition buttonPosition) {
         // Если пользователь указал старую позицию, ничего делать не будет.
@@ -220,47 +208,39 @@ public class FileEdit extends JPanel {
             return;
         }
 
-        // Запонимаем новую позицию
         this.buttonPosition = buttonPosition;
 
-        // Теперь нужно разместить кнопку на новом месте. Для этого удаляем ее
-        // и размещаем заново, с новой позицией.
         remove(button);
         add(button, this.buttonPosition.getBorderLayoutConstant());
     }
 
     /**
-     * Устанавливает заголовок кнопки. В случае, если текст кнопки был изменен
-     * напрямую через доступ к переменной кнопки, данное значение теряется.
+     * Set button text.
      * 
-     * @param text -
-     *            новый текст, отображаемый на кнопке.
+     * @param text
      */
     public void setButtonText(String text) {
         button.setText(text);
     }
 
     /**
-     * Возвращает установленный заголовок кнопки.
+     * Return button title.
      * 
-     * @return - заголовок кнопки.
+     * @return
      */
     public String getButtonText() {
         return button.getText();
     }
 
     /**
-     * Для корректного позиционировая компонент, требуется чтобы менеджером
-     * раскладки обязательно был BorderLayout. В ином случае кидаем исключение
-     * {@link IllegalArgumentException}
+     * This component required only {@link BorderLayout} manager. You shouldn't
+     * use this method.
      * 
-     * @param -
-     *            экземпляр класса {@link BorderLayout}
-     * @throws IllegalArgumentException -
-     *             в случае, если менеждер раскладки не является экземпляром
-     *             класса BorderLayout.
+     * @throws IllegalArgumentException
+     *             if mgr is not instance of {@link BorderLayout}.
      */
     @Override
+    @Deprecated
     final public void setLayout(LayoutManager mgr) {
         if (!(mgr instanceof BorderLayout)) {
             throw new IllegalArgumentException(
@@ -270,38 +250,21 @@ public class FileEdit extends JPanel {
         }
     }
 
-    /**
-     * Добавляет в список слушателей событий слушателя выбора имени файла.
-     * 
-     * @param l -
-     *            слушатель имени файла.
-     */
     public void addFileSelectedListener(FileSelectedListener l) {
         listenerList.add(FileSelectedListener.class, l);
     }
 
-    /**
-     * Удаляет из списка слушателей событий слушателя выбора имени файла
-     * 
-     * @param l
-     */
     public void removeFileSelectedListener(FileSelectedListener l) {
         listenerList.remove(FileSelectedListener.class, l);
     }
 
-    /**
-     * Возвращает массив зарегестрированных слушателей события выбора файла
-     * 
-     * @return - массив слушателей события выбора файла.
-     */
     public FileSelectedListener[] getFileSelectedListeners() {
         return (FileSelectedListener[]) listenerList
                 .getListeners(FileSelectedListener.class);
     }
 
     /**
-     * Вызывает событие изменения выбора файла.
-     * 
+     * It's fire whene file name is changed.
      */
     private void fireFileSelectedEvent() {
         FileSelectedEvent event = new FileSelectedEvent(this);
